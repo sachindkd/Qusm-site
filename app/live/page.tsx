2b97cf52f98e7250f7bbf4371a122f65aa9cc9f0
sachindkd <165709511+sachindkd@users.noreply.github.com>
2026-08-31 19:56:33 +0400

    Render investigation agencies below NSA

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,8 +1,9 @@
 import LiveHomeRevampFixed from "./LiveHomeRevampFixed";
 import NSASection from "./NSASection";
+import InvestigationAgenciesSection from "./InvestigationAgenciesSection";
 
 export const dynamic = "force-dynamic";
 
 export default function LivePage() {
-  return <><LiveHomeRevampFixed /><NSASection /></>;
+  return <><LiveHomeRevampFixed /><NSASection /><InvestigationAgenciesSection /></>;
 }

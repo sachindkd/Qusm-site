2d08a73a2bcae97aa7d40b5421e9a8034f1f6eb3
sachindkd <165709511+sachindkd@users.noreply.github.com>
2026-08-31 19:21:35 +0400

    Render NSA investigations section

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,7 +1,8 @@
 import LiveHomeRevampFixed from "./LiveHomeRevampFixed";
+import NSASection from "./NSASection";
 
 export const dynamic = "force-dynamic";
 
 export default function LivePage() {
-  return <LiveHomeRevampFixed />;
+  return <><LiveHomeRevampFixed /><NSASection /></>;
 }
